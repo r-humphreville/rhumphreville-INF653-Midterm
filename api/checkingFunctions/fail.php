d667ae737a6a31c535f637ddd6d76667656172b3
r-humphreville <101674402+r-humphreville@users.noreply.github.com>
2022-03-23 17:47:25 -0500

    Add files via upload

---
+++ api/checkingFunctions/fail.php
@@ -0,0 +1,5 @@
+<?php
+function fail(String $modelType, String $op) {
+ echo json_encode(array('message' => $modelType . ' Not ' . $op));
+}
+?>
---
+++ api/checkingFunctions/missingReqParams.php
@@ -0,0 +1,5 @@
+<?php
+function missingParams() {
+    echo json_encode(array('message' => 'Missing Required Parameters'));
+}
+?>
